9b718302c6119cb376adb85303091fb48be818e8
Wilfred <wilfred05777@gmail.com>
2022-12-08 14:53:51 +0800

    fixing image size adn width of login ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 // https://www.youtube.com/watch?v=k4mjF4sPITE&list=PLj-4DlPRT48nfYgDK00oTjlDF4O0ZZyG8&index=36
+
+// https://github.com/safak/youtube2022/tree/react-chat
+
 import "./App.css";
 import Register from "./pages/Register";
 import "./style.scss";
