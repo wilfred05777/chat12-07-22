0c712db837446bec6a26b6b9b0f77ca62ec2005d
Wilfred <wilfred05777@gmail.com>
2022-12-08 11:26:48 +0800

    0:05:00 Start Form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Chat Bot2022</span>
+        <span className="title">Register</span>
+
+        <form>
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="file" />
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
