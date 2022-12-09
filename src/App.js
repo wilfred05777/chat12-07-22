784fa7ae571338f08e331e62f6016ccc92d18392
Wilfred <wilfred05777@gmail.com>
2022-12-09 17:29:15 +0800

    1:05:19 React Router Dom configuration

--- src/App.js
+++ src/App.js
@@ -8,8 +8,20 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.scss";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
